bb3ebded54efa6702d0aeb9acbeb113e73b5287b
TahaSyedKhan <tahasyedkhan123@gmail.com>
2026-01-14 15:31:15 +0530

    added functionality to tweet

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 import { tweetsData } from './tweetsData.js'
 
-const feedELe = document.getElementById('feed')
-const tweetBtn = document.getElementById('tweet-btn')
-const tweetInput = document.getElementById('tweet')
-
-tweetBtn.addEventListener('click', function() {
-    console.log(tweetInput.value)
-})
-
 document.addEventListener('click', function(e) {
 
     if(e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like)
     } 
         else if (e.target.dataset.retweet) {
-        handleRetweetClick(e.target.dataset.retweet)
+            handleRetweetClick(e.target.dataset.retweet)
+    } 
+        else if (e.target.dataset.reply) {
+            handleReplyClick(e.target.dataset.reply)
+    }
+    else if (e.target.id == 'tweet-btn') {
+        handleTweetBtnClick()
     }
 })
 
@@ -49,15 +47,60 @@ function handleRetweetClick(tweetId) {
     render()
 }
 
+function handleReplyClick(replyId) {
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleTweetBtnClick() {
+
+    const tweetInput = document.getElementById('tweet')
+
+    if(tweetInput.value) {
+        tweetsData.unshift({
+        handle: `@scrimba`,
+        profilePic: `images/scrimbalogo.png`,
+        likes: 0,
+        retweets: 0,
+        tweetText: tweetInput.value,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: crypto.randomUUID()
+        })
+    render()
+    tweetInput.value = ''
+    }
+}
+
 function getFeedHtml() {
 
-    let feedHtml = tweetsData.map( tweet => {
+    let feedHtml = ''
+
+    tweetsData.forEach( tweet => {
 
         const likeIconClass = tweet.isLiked ? 'liked' : ''
 
         const retweetIconClass = tweet.isRetweeted ? 'retweeted' : ''
 
-    return `
+        let repliesHtml = ''
+
+        if (tweet.replies) {
+            tweet.replies.forEach( reply => {
+                repliesHtml += `
+                        <div class="tweet-reply">
+                            <div class="tweet-inner">
+                                <img src="${reply.profilePic}" class="profile-pic">
+                                    <div>
+                                        <p class="handle">${reply.handle}</p>
+                                        <p class="tweet-text">${reply.tweetText}</p>
+                                    </div>
+                                </div>
+                        </div>
+                `
+            })
+        }
+
+    feedHtml += `
                 <div class="tweet">
                 <div class="tweet-inner">
                     <img src="${tweet.profilePic}" class="profile-pic">
@@ -66,7 +109,7 @@ function getFeedHtml() {
                         <p class="tweet-text">${tweet.tweetText}</p>
                         <div class="tweet-details">
                             <span class="tweet-detail">
-                                <i class="fa-regular fa-comment-dots"></i>
+                                <i class="fa-regular fa-comment-dots"data-reply=${tweet.uuid}></i>
                                 ${tweet.replies.length}
                             </span>
                             <span class="tweet-detail">
@@ -80,6 +123,9 @@ function getFeedHtml() {
                         </div>   
                     </div>            
                 </div>
+                <div class="hidden" id="replies-${tweet.uuid}">
+                    ${repliesHtml}
+                </div> 
             </div>
     `
 })
@@ -88,6 +134,7 @@ function getFeedHtml() {
 
 
 function render() {
+    const feedELe = document.getElementById('feed')
     feedELe.innerHTML = getFeedHtml()
 }
 
